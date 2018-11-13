ed09256e44dd94f36c2adc84b59d028ba77d783b
Renato Prata <renprata@hotmail.com>
2018-11-13 07:08:04 -0300

    Curso 3 - Android

--- app/src/main/java/br/com/alura/agenda/MapaFragment.java
+++ app/src/main/java/br/com/alura/agenda/MapaFragment.java
@@ -46,6 +46,8 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
             }
         }
         alunoDAO.close();
+
+        new Localizador(getContext(), googleMap);
     }
 
     private LatLng pegaCoordenadaDoEndereco(String endereco) {
